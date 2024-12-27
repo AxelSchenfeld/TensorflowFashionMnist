52281b2bf830d9b11ae8bd4645194f236431a2c2
Axel Schenfeld <106484712+AxelSchenfeld@users.noreply.github.com>
2024-12-27 14:19:32 -0300

    Add files via upload

--- fachionMnist.py
+++ fachionMnist.py
@@ -75,7 +75,7 @@ x_train = x_train.reshape((x_train.shape[0], 28, 28, 1))
 x_test = x_test.reshape((x_test.shape[0], 28, 28, 1))
 
 # Entrenar el modelo
-history = model.fit(x_train, y_train, epochs=10, validation_split=0.2)
+history = model.fit(x_train, y_train, batch_size=256, epochs=10,verbose=1, validation_split=0.2)
 
 # Evaluar el modelo
 loss, accuracy = model.evaluate(x_test, y_test)
